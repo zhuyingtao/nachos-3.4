a8c4a926dda617bf2f9914404c7f60a576ad7cd9
zhuyingtao <yingtao.zhu@foxmail.com>
2015-09-11 17:53:10 +0800

    First commit

---
+++ code/test/newhalt.c
@@ -0,0 +1,26 @@
+/* halt.c
+ *	Simple program to test whether running a user program works.
+ *	
+ *	Just do a "syscall" that shuts down the OS.
+ *
+ * 	NOTE: for some reason, user programs with global data structures 
+ *	sometimes haven't worked in the Nachos environment.  So be careful
+ *	out there!  One option is to allocate data structures as 
+ * 	automatics within a procedure, but if you do this, you have to
+ *	be careful to allocate a big enough stack to hold the automatics!
+ */
+
+#include "syscall.h"
+static int a[80];
+int
+main()
+{
+    int i,j,k;
+    k=3;
+    i=2;
+    j=1;
+    k=i;
+    i=j+k;
+    Halt();
+    /* not reached */
+}
---
+++ code/test/newhalt0.s
@@ -0,0 +1,36 @@
+	.file	1 "newhalt.c"
+gcc2_compiled.:
+__gnu_compiled_c:
+	.text
+	.align	2
+	.globl	main
+	.ent	main
+main:
+	.frame	$fp,40,$31		# vars= 16, regs= 2/0, args= 16, extra= 0
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	subu	$sp,$sp,40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	move	$fp,$sp
+	jal	__main
+	li	$2,3			# 0x00000003
+	sw	$2,24($fp)
+	li	$2,2			# 0x00000002
+	sw	$2,16($fp)
+	li	$2,1			# 0x00000001
+	sw	$2,20($fp)
+	lw	$2,16($fp)
+	sw	$2,24($fp)
+	lw	$2,20($fp)
+	lw	$3,24($fp)
+	addu	$2,$2,$3
+	sw	$2,16($fp)
+	jal	Halt
+$L1:
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	addu	$sp,$sp,40
+	j	$31
+	.end	main
